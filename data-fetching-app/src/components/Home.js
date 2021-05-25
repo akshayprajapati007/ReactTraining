4c1e66e7476aaa59a63155adbdde648d3c7ab5a5
Sanjay Bikhchandani <sanjay.bikhchandani@tatvasoft.com>
2021-05-25 19:12:27 +0530

    assignment 2 completed on 25/05/21

--- data-fetching-app/src/components/Home.js
+++ data-fetching-app/src/components/Home.js
@@ -1,38 +1,60 @@
-import React, { Component } from 'react'
-import TableData from './TableData'
+import { Component } from 'react'
+import TableData from './TableData';
+
 
 export class Home extends Component {
 
+    filterArray = []
+    interval = 0
     constructor(props) {
         super(props)
 
         this.state = {
             displayData: [],
-            pageNumber:0
+            pageNumber:0,
+            searchValue: ''
         }
     }
 
     componentDidMount() {
+
+        //fetching data from api
         const getData = async () => {
             const response = await fetch(`https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${this.state.pageNumber}`)
             const data = await response.json()
-            console.log(data)
-            this.setState({
+            this.setState(prevState => ({
                 pageNumber : this.state.pageNumber + 1,
-                displayData : data
-            })
+                displayData : [...prevState.displayData, ...data.hits]
+            }))
+            this.filterArray = data.hits
         } 
+
         getData()
-        /* setInterval( () => {
+        /* this.interval = setInterval( () => {
             getData()
         }, 10000);  */
+    }
+
+    searchHandler(e){
+        let filterArray2 = this.filterArray.filter(d => {
+        let x = d.title.toLowerCase()
+        return x.indexOf(e.target.value) !== -1
+        })
+        this.setState({
+             displayData : filterArray2
+        })
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
     }
 
     render() {
 
         return (
-            <div>
-                {this.state.displayData?.hits && <TableData alldata={this.state.displayData}/>}
+            <div className='main-wrapper'>
+                    <input type='text' placeholder='Search..' value={this.state.value} onChange={this.searchHandler.bind(this)} class='table-search'></input>
+                {this.state.displayData && <TableData alldata={this.state.displayData} page_number={this.state.pageNumber}/>}
             </div>
         )
     }
--- data-fetching-app/src/components/TableData.js
+++ data-fetching-app/src/components/TableData.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import TablePagination from '@material-ui/core/TablePagination';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
         '& > *': {
           margin: theme.spacing(1),
+          marginTop: theme.spacing(2),
         },
       },
     table: {
@@ -39,32 +43,63 @@ const StyledTableRow = withStyles((theme) => ({
 
 function TableData(alldata) {
 
+    let num = 0  
+
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [orderDirection, setOrderDirection] = React.useState('asc');
+    const [valueToOrderBy, setValueToOrderBy]= React.useState(0)
     const classes = useStyles();
-    console.log(alldata.alldata.hits)
-    const data1 = alldata.alldata.hits.map(c => 'hi')
+
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    };
 
     return (
-        <div className='data-table'>
-            <TableContainer component={Paper}>
+        <div >
+            <TableContainer component={Paper} className='data-table'>
                 <Table className={classes.table} aria-label="customized table">
                     <TableHead>
                         <TableRow>
-                            <StyledTableCell align="center">Title</StyledTableCell>
+                            <StyledTableCell align="center">No.</StyledTableCell>
+                            <StyledTableCell align="center">
+                                <TableSortLabel 
+                                  active={'title' === 'title'}
+                                  direction='asc'
+                                  //onClick={createSortHandler('title')}
+                                >
+                                  Title
+                                </TableSortLabel>
+                            </StyledTableCell>
                             <StyledTableCell align="center">URL</StyledTableCell>
                             <StyledTableCell align="center">Created Date</StyledTableCell>
                             <StyledTableCell align="center">Author</StyledTableCell>
                         </TableRow>
                     </TableHead> 
                     <TableBody>
-                        {alldata.alldata.hits.map(dt => 
-                            <StyledTableRow>
-                            <StyledTableCell component="th" scope="row" align="center">{dt.title}</StyledTableCell>
-                            <StyledTableCell align="center">{dt.url}</StyledTableCell>
+                        {alldata.alldata.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(dt => 
+                            <StyledTableRow key={num}>
+                            <StyledTableCell align="center">{++num}</StyledTableCell>
+                            <StyledTableCell align="center">{dt.title}</StyledTableCell>
+                            <StyledTableCell align="center">{dt.url ? dt.url : 'Null'}</StyledTableCell>
                             <StyledTableCell align="center">{dt.created_at}</StyledTableCell>
                             <StyledTableCell align="center">{dt.author}</StyledTableCell>
                         </StyledTableRow>)}
                     </TableBody>
-                </Table>
+                </Table>        
+                <TablePagination
+                      component='div'
+                      count={alldata.alldata.length}
+                      page={page}
+                      onChangePage={handleChangePage}
+                      rowsPerPage={rowsPerPage}
+                      onChangeRowsPerPage={handleChangeRowsPerPage}
+                    />        
             </TableContainer>
         </div>
     )
